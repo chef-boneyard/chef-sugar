4fcbca6b4310d86beff7db96a9ab935b5a4fe9ed
Seth Vargo <sethvargo@gmail.com>
2014-10-12 10:54:38 -0400

    Use expand_path because require_relative doesn't work :)

--- metadata.rb
+++ metadata.rb
@@ -14,5 +14,5 @@ For the most up-to-date information and documentation, please visit the [Chef
 Sugar project page on GitHub](https://github.com/sethvargo/chef-sugar).
 EOH
 
-require_relative 'lib/chef/sugar/version'
+require          File.expand_path('../lib/chef/sugar/version', __FILE__)
 version          Chef::Sugar::VERSION
